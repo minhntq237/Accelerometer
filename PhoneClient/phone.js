a6081182d28e4c95b70cc478af3e8a594074f37c
Nguyen Minh <minhntq237@gmail.com>
2022-05-03 21:27:20 +0700

    fix websocket

--- PhoneClient/phone.js
+++ PhoneClient/phone.js
@@ -100,10 +100,13 @@ module.exports = new RotationInfoList()
 //Phone UI
 },{"./WebsocketConnection.js":2}],2:[function(require,module,exports){
 const websocketServerPortNumber = 8500
+const localWebsocketServer = `ws://localhost:${websocketServerPortNumber}/`
+const outsideWebsocketServer = `ws://http://159.223.43.104:${websocketServerPortNumber}/`
+
 
 class WebsocketConnection {
     constructor(thisWebsocketClass) {
-        this.websocket = new thisWebsocketClass(`ws://localhost:${websocketServerPortNumber}/`)
+        this.websocket = new thisWebsocketClass(outsideWebsocketServer)
     }
     sendDataToWebSocketServer(thisMessage, thisContent) {
         this.websocket.send(JSON.stringify({message: thisMessage, content: thisContent}));
